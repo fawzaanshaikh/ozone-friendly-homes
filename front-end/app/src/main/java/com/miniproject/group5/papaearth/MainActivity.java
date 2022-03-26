63354bea4532715c9c0e9f7c33b9dc548bcd2c9a
Fawzaan <fawzi20112001@gmail.com>
2022-03-26 13:12:25 +0530

    Front page prototype complete

--- front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
+++ front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
@@ -3,14 +3,24 @@ package com.miniproject.group5.papaearth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnTakePhoto;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTakePhoto = findViewById(R.id.btnTakePhoto);
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView sampleText = (TextView) findViewById(R.id.sampleText);
+                sampleText.setText("Button click.");
+            }
+        });
     }
 }
