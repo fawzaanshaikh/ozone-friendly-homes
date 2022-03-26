1c17d69e10ef17c8b48689fc804ea67546f49fb1
Fawzaan <fawzi20112001@gmail.com>
2022-03-26 15:05:10 +0530

    Convert images to bitmap

--- front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
+++ front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
@@ -3,13 +3,16 @@ package com.miniproject.group5.papaearth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button btnTakePhoto;
+    private static final int CAMERA_PIC_REQUEST = 1337;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +24,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 TextView sampleText = (TextView) findViewById(R.id.sampleText);
                 sampleText.setText("Opening Camera...");
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivity(intent);
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            ImageView imageview = (ImageView) findViewById(R.id.imageView2); //sets imageview as the bitmap
+            imageview.setImageBitmap(image);
+        }
     }
 }
