dd32776e31affe177a1c9caf3ab027f04343cf54
Fawzaan <fawzi20112001@gmail.com>
2022-03-26 12:18:51 +0530

    Front page created

--- front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
+++ front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
@@ -3,9 +3,11 @@ package com.miniproject.group5.papaearth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnTakePhoto;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
