42e1619ed7704f7953fd70640d287024ebd4b25c
Fawzaan <fawzi20112001@gmail.com>
2022-03-26 14:15:32 +0530

    Removed app bar and added camera functionality

--- front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
+++ front-end/app/src/main/java/com/miniproject/group5/papaearth/MainActivity.java
@@ -2,6 +2,7 @@ package com.miniproject.group5.papaearth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
         btnTakePhoto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 TextView sampleText = (TextView) findViewById(R.id.sampleText);
-                sampleText.setText("Button click.");
+                sampleText.setText("Opening Camera...");
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
             }
         });
     }
